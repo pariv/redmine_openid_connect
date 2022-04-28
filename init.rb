38068abe402416c4ec562143cb1fff29e50a5f9f
pariv <pariv@local.yurion.ru>
2022-04-28 08:49:38 +0300

    исправление запуска хуков

--- init.rb
+++ init.rb
@@ -3,18 +3,21 @@ require_relative 'lib/redmine_openid_connect/application_controller_patch'
 require_relative 'lib/redmine_openid_connect/account_controller_patch'
 require_relative 'lib/redmine_openid_connect/hooks'
 
+loader = RedminePluginKit::Loader.new plugin_id: 'redmine_openid_connect'
+
 Redmine::Plugin.register :redmine_openid_connect do
   name 'Redmine Openid Connect plugin'
   author 'Alfonso Juan Dillera / Markus M. May'
   description 'OpenID Connect implementation for Redmine'
-  version '0.9.4'
+  version '0.9.4.1'
   url 'https://github.com/devopskube/redmine_openid_connect'
   author_url 'http://github.com/adillera'
 
   settings :default => { 'empty' => true }, partial: 'settings/redmine_openid_connect_settings'
 end
 
-Rails.configuration.to_prepare do
-  ApplicationController.prepend(RedmineOpenidConnect::ApplicationControllerPatch)
-  AccountController.prepend(RedmineOpenidConnect::AccountControllerPatch)
-end
+RedminePluginKit::Loader.persisting { loader.load_model_hooks! }
+RedminePluginKit::Loader.to_prepare { 
+    ApplicationController.prepend(RedmineOpenidConnect::ApplicationControllerPatch)
+    AccountController.prepend(RedmineOpenidConnect::AccountControllerPatch)
+    } if Rails.version < '6.0'
